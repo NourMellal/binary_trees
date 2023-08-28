74d4e159de3faf5c597123d23aec165b2dfa5457
NourMellal <mellal.nour9@gmail.com>
2023-08-28 23:48:44 +0100

    Measures the balance factor of a binary tree.

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,6 +8,21 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	if (!tree)
+		return (0);
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+}
+
+/**
+ * max - Find maximum between two numbers.
+ *
+ * Return: maximum number
+ * @num1: int 1
+ * @num2: int 2
+ */
+size_t max(int num1, int num2)
+{
+	return ((num1 > num2) ? num1 : num2);
 }
 
 /**
@@ -17,7 +32,12 @@ int binary_tree_balance(const binary_tree_t *tree)
  *
  * Return: 0 if tree is NULL.
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	if (!tree)
+		return (-1);
+	return (max(binary_tree_height(tree->left)
+												, binary_tree_height(tree->right)) + 1);
 }
 
