d2023ef4f7321c89a352e1f1e765fd54ef6010b1
NourMellal <mellal.nour9@gmail.com>
2023-08-29 17:55:10 +0100

    Finds the sibling of a node.

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,5 +8,17 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	binary_tree_t *parent;
+
+	if (!node || !node->parent)
+		return (NULL);
+	parent = node->parent;
+
+	if (parent->left == node)
+		return (parent->right);
+	else if (parent->right == node)
+		return (parent->left);
+	else
+		return (NULL);
 }
 
