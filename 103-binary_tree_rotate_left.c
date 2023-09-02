6d813f29f80498d31654d62d1309e91604b1c099
NourMellal <mellal.nour9@gmail.com>
2023-09-02 13:35:49 +0100

    Performs a left-rotation on a binary tree.

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -8,5 +8,26 @@
  */
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
-}
+	binary_tree_t *new_root, *tmp;
 
+	if (!tree || !tree->right)
+		return (tree);
+
+	new_root = tree->right;
+	tmp = new_root->left;
+	new_root->left = tree;
+	tree->right = tmp;
+	if (tmp)
+		tmp->parent = tree;
+	tmp = tree->parent;
+	tree->parent = new_root;
+	new_root->parent = tmp;
+	if (tmp)
+	{
+		if (tmp->left == tree)
+			tmp->left = new_root;
+		else
+			tmp->right = new_root;
+	}
+	return (new_root);
+}
